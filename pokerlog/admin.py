a3927f73e2e5db5ce52dc4c9be794738e7a2f956
Aran Patel <aranlucapatel1@gmail.com>
2026-03-27 16:25:16 +0000

    fix admin ev_profit reference

--- pokerlog/admin.py
+++ pokerlog/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import Entry, Profile
+from .models import Entry, Profile, Venue
 
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ("user", "played_at", "format", "buy_in", "cash_out", "ev_profit")
-    list_filter = ("format", "played_at")
+    list_display = ("user", "played_at", "format", "currency", "venue", "buy_in", "cash_out")
+    list_filter = ("format", "played_at", "venue")
     search_fields = ("user__username", "notes")
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "starting_bankroll", "max_buyin_pct", "stop_loss_buyins")
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "created_at")
+    search_fields = ("user__username", "name")
